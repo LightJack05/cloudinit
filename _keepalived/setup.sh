13c8a346fa6c9ac3579357a4a693add88e3d4eff
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-11-30 16:08:36 +0100

    Changed paths to absolute paths

--- _keepalived/setup.sh
+++ _keepalived/setup.sh
@@ -2,13 +2,10 @@
 # Install keepalived
 apt install keepalived -y
 
-# Create the config
-mkdir /etc/keepalived/
-
 # Download required files
 curl http://cloudinit.lightjack.lan/_keepalived/config_template.conf -o /etc/keepalived/keepalived.conf
-curl http://cloudinit.lightjack.lan/_keepalived/variables.sh -o ~/variables.sh
-curl http://cloudinit.lightjack.lan/_keepalived/init_node.sh -o ~/init_node.sh
-chmod +x ~/*.sh
+curl http://cloudinit.lightjack.lan/_keepalived/variables.sh -o /root/variables.sh
+curl http://cloudinit.lightjack.lan/_keepalived/init_node.sh -o /root/init_node.sh
+chmod +x /root/*.sh
 
 
