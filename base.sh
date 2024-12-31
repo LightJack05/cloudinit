bd96a24e2bae9c779cc4524dd41f153d4bfe38f2
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-12-31 13:17:22 +0100

    Corrected cloudinit crontabs

--- base.sh
+++ base.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Set up auto updates
-(crontab -l 2>/dev/null; echo "10 3 * * 1 date >> /var/log/cron.log && echo \"Starting maintainance crontabs...\" >> cron.log ") | crontab -
+(crontab -l 2>/dev/null; echo "10 3 * * 1 date >> /var/log/cron.log && echo \"Starting maintainance crontabs...\" >> /var/log/cron.log ") | crontab -
 (crontab -l 2>/dev/null; echo "10 3 * * 1 apt update && apt upgrade -y >> /var/log/cron.log") | crontab -
 (crontab -l 2>/dev/null; echo "20 3 * * 1 echo \"Ran maintainance crontabs\" >> /var/log/cron.log") | crontab -
 
