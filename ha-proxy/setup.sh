99884b078e7ab962d6dbc3407451f425b7fe08b3
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-01-11 20:42:50 +0100

    Fixed filename

--- ha-proxy/setup.sh
+++ ha-proxy/setup.sh
@@ -3,6 +3,6 @@
 apt install haproxy -y
 
 # Download required files
-curl http://cloudinit.lightjack.lan/ha-proxy/config_template.conf -o /etc/haproxy/haproxy.cfg 
+curl http://cloudinit.lightjack.lan/ha-proxy/config_template.cfg -o /etc/haproxy/haproxy.cfg 
 curl http://cloudinit.lightjack.lan/ha-proxy/variables.sh >> /root/variables.sh
 curl http://cloudinit.lightjack.lan/ha-proxy/init_node.sh >> /root/init_node.sh
